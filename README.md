da59e34ebf598f4ab2c731c10e9c62d20a2a6854
monu-shaw <premraj00071@gmail.com>
2022-08-06 20:42:29 +0530

    express Api Link Previewd

--- README.md
+++ README.md
@@ -1,23 +1,29 @@
-#Link Previewer By Monu Shaw
+# Link Previewer By Monu Shaw
 Made With pupeteer and cherio.
 ##Exclusive made Findcoder.io Challenge 
 challenge link (https://www.findcoder.io/challenges)
 
-##How To use 
-npm i monu-linkpreview \n
+## How To use 
+npm i monu-linkpreview. 
+```
+import getLinkPreview from 'monu-linkpreview'; 
 
-import getLinkPreview from 'monu-linkpreview';
-import React, {useState, useEffect} from 'react'
+getLinkPreview(url).then(res=>log(res)).catch((err)=>console.log(err));
+```
+## Important 
+It will Work In Node. \
+You Need To Use an Api Call \
+example (Used Express)
+```
+app.post('/',  async (req, res) => {
+        try {
+             await main(req.body.url).then(re=>res.send(re)).catch((er)=>res.send(er)));
+        } catch (error) {
+            res.send(`error ${error}`)
+        }
+})
+```
+Full Example Code Here (Soon)
+## Demo Here
 
-
-export default function Heading() {
-  const [link, setLink] = useState([]);
-  useEffect(()=>{
-    getLinkPreview('https://webscraper.io/test-sites').then(res=>setLink(res));
-  },[])
-  return (
-
-  )
-}
-
-##Demo Here
+Here (https://linkpreview-alpha.vercel.app)
--- index.js
+++ index.js
@@ -1,145 +1,143 @@
-import puppeteer from 'puppeteer';
 
-import cheerio from 'cheerio';
+
+import * as cheerio from 'cheerio';
+import axios from 'axios';
 
 export default async function getLinkPreview(url) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: []
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle2', timeout: 0});
-  const content = await page.content();
-  const $ = cheerio.load(content);
+  const content = await axios(url).then((re)=>{
+    const $ = cheerio.load(re.data); 
 
-
-
-  browser.close();
-  
-  const data = (()=>{
+    const data = (()=>{
     
-  const img =  ( () => {
-    const ogImg = $('meta[property="og:image"]');
-    if (
-      ogImg.attr('content') != null &&
-      ogImg.attr('content').length > 0
-    ) {
-      return ogImg.attr('content');
-    }
-    const twitterImg = $('meta[name="twitter:image"]');
-    if (
-      twitterImg.attr('content') != null &&
-      twitterImg.attr('content').length > 0 
-    ) {
-      return twitterImg.attr('content');
-    }
-    const imgRelLink = $('link[rel="icon"]');
-    if (
-      imgRelLink.attr('href') != null &&
-      imgRelLink.attr('href').length > 0 
-    ) {
-      return url+imgRelLink.attr('href');
-    }
-
-    let imgs = Array.from($("img"));
-    if (imgs.length > 0) {
-      imgs = imgs.filter(img => {
-        let addImg = true;
-        if (img.naturalWidth > img.naturalHeight) {
-          if (img.naturalWidth / img.naturalHeight > 3) {
-            addImg = false;
-          }
-        } else {
-          if (img.naturalHeight / img.naturalWidth > 3) {
-            addImg = false;
-          }
+      const img =  ( () => {
+        const ogImg = $('meta[property="og:image"]');
+        if (
+          ogImg.attr('content') != null &&
+          ogImg.attr('content').length > 0
+        ) {
+          const imgOrigin = new URL(url).origin;
+          return ogImg.attr('content').match(/(http(s)?)\:\/\/(www.)?(facebook.com\/)?(pages\/)?/gm)?ogImg.attr('content'): imgOrigin+ogImg.attr('content');
         }
-        if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
-          addImg = false;
+        const twitterImg = $('meta[name="twitter:image"]');
+        if (
+          twitterImg.attr('content') != null &&
+          twitterImg.attr('content').length > 0 
+        ) {
+          return twitterImg.attr('content');
         }
-        return addImg;
+        const imgRelLink = $('link[rel="icon"]');
+        if (
+          imgRelLink.attr('href') != null &&
+          imgRelLink.attr('href').length > 0 
+        ) {
+          const imgOrigin = new URL(url).origin;
+          return imgRelLink.attr('href').match(/(http(s)?)\:\/\/(www.)?(facebook.com\/)?(pages\/)?/gm)?imgRelLink.attr('href'): imgOrigin+imgRelLink.attr('href');
+        }
+    
+        let imgs = Array.from($("img"));
+        if (imgs.length > 0) {
+          imgs = imgs.filter(img => {
+            let addImg = true;
+            if (img.naturalWidth > img.naturalHeight) {
+              if (img.naturalWidth / img.naturalHeight > 3) {
+                addImg = false;
+              }
+            } else {
+              if (img.naturalHeight / img.naturalWidth > 3) {
+                addImg = false;
+              }
+            }
+            if (img.naturalHeight <= 50 || img.naturalWidth <= 50) {
+              addImg = false;
+            }
+            return addImg;
+          });
+          /* imgs.forEach(img =>
+            img.src.indexOf("//") === -1
+              ? (img.src = `${new URL(url).origin}/${src}`)
+              : img.src
+          ); */
+          return imgs[0].src === undefined? null:null;
+        }
+        return null;
       });
-      /* imgs.forEach(img =>
-        img.src.indexOf("//") === -1
-          ? (img.src = `${new URL(url).origin}/${src}`)
-          : img.src
-      ); */
-      return imgs[0].src === undefined? null:imgs[0].src;
-    }
-    return null;
+    
+      const title = (() => {
+        const ogTitle = $('meta[property="og:title"]');
+        if (ogTitle.attr('content') != null && ogTitle.attr('content').length > 0) {
+          return ogTitle.attr('content');
+        }
+        const twitterTitle = $('meta[name="twitter:title"]');
+        if (twitterTitle.attr('content') != null && twitterTitle.attr('content').length > 0) {
+          return twitterTitle.attr('content');
+        }
+        const docTitle = $('title').text();
+        if (docTitle != null && docTitle.length > 0) {
+          return docTitle;
+        }
+        const h1 = $("h1").text();
+        if (h1 != null && h1.length > 0) {
+          return h1;
+        }
+        const h2 = $("h1").text();
+        if (h2 != null && h2.length > 0) {
+          return h2;
+        }
+        return null;
+      });
+    
+      const description =(() => {
+        const ogDescription = $(
+          'meta[property="og:description"]'
+        );
+        if (ogDescription.attr('content') != null && ogDescription.attr('content').length > 0) {
+          return ogDescription.attr('content');
+        }
+        const twitterDescription = $(
+          'meta[name="twitter:description"]'
+        );
+        if (twitterDescription.attr('content') != null && twitterDescription.attr('content').length > 0) {
+          return twitterDescription.attr('content');
+        }
+        const metaDescription = $('meta[name="description"]');
+        if (metaDescription.attr('content') != null && metaDescription.attr('content').length > 0) {
+          return metaDescription.attr('content');
+        }
+        let paragraphs = $("p");
+        let fstVisibleParagraph = null;
+        /* for (let i = 0; i < paragraphs.length; i++) {
+          if (
+            // if object is visible in dom
+            paragraphs[i].offsetParent() !== null &&
+            !paragraphs[i].children().length != 0
+          ) {
+            fstVisibleParagraph = paragraphs[i].text();
+            break;
+          }
+        } */
+        return fstVisibleParagraph;
+      });
+    
+      
+        const domainName = (() => {
+          const canonicalLink = $("link[rel=canonical]");
+          if (canonicalLink.attr('href') != null && canonicalLink.attr('href').length > 0) {
+            return canonicalLink.attr('href');
+          }
+          const ogUrlMeta = $('meta[property="og:url"]');
+          if (ogUrlMeta.attr('content') != null && ogUrlMeta.attr('content').length > 0) {
+            return ogUrlMeta.attr('content');
+          }
+          return new URL(url).origin;
+        });
+        const origin = (() => {
+          return new URL(url).origin;
+        });
+    
+     return  {"img": img(), "title": title(), "description": description(), "domain":domainName(), "origin": origin() };
+      })
+      return data();
   });
-
-  const title = (() => {
-    const ogTitle = $('meta[property="og:title"]');
-    if (ogTitle.attr('content') != null && ogTitle.attr('content').length > 0) {
-      return ogTitle.attr('content');
-    }
-    const twitterTitle = $('meta[name="twitter:title"]');
-    if (twitterTitle.attr('content') != null && twitterTitle.attr('content').length > 0) {
-      return twitterTitle.attr('content');
-    }
-    const docTitle = $('title').text();
-    if (docTitle != null && docTitle.length > 0) {
-      return docTitle;
-    }
-    const h1 = $("h1").text();
-    if (h1 != null && h1.length > 0) {
-      return h1;
-    }
-    const h2 = $("h1").text();
-    if (h2 != null && h2.length > 0) {
-      return h2;
-    }
-    return null;
-  });
-
-  const description =(() => {
-    const ogDescription = $(
-      'meta[property="og:description"]'
-    );
-    if (ogDescription.attr('content') != null && ogDescription.attr('content').length > 0) {
-      return ogDescription.attr('content');
-    }
-    const twitterDescription = $(
-      'meta[name="twitter:description"]'
-    );
-    if (twitterDescription.attr('content') != null && twitterDescription.attr('content').length > 0) {
-      return twitterDescription.attr('content');
-    }
-    const metaDescription = $('meta[name="description"]');
-    if (metaDescription.attr('content') != null && metaDescription.attr('content').length > 0) {
-      return metaDescription.attr('content');
-    }
-    paragraphs = $("p");
-    let fstVisibleParagraph = null;
-    for (let i = 0; i < paragraphs.length; i++) {
-      if (
-        // if object is visible in dom
-        paragraphs[i].offsetParent !== null &&
-        !paragraphs[i].childElementCount != 0
-      ) {
-        fstVisibleParagraph = paragraphs[i].textContent;
-        break;
-      }
-    }
-    return fstVisibleParagraph;
-  });
-
-  
-    const domainName = (() => {
-      const canonicalLink = $("link[rel=canonical]");
-      if (canonicalLink.attr('href') != null && canonicalLink.attr('href').length > 0) {
-        return canonicalLink.attr('href');
-      }
-      const ogUrlMeta = $('meta[property="og:url"]');
-      if (ogUrlMeta.attr('content') != null && ogUrlMeta.attr('content').length > 0) {
-        return ogUrlMeta.attr('content');
-      }
-      return url ;
-    });
-
- return  {"img": img(), "title": title(), "description": description(), "domain":domainName()};
-  })
-  return data();
+  return content;
 }
 
